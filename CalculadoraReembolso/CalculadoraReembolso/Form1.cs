409882bdb2519ada4dde7ba8460b5e725e59e09e
LucasCosendey <lcosendey@gmail.com>
2021-09-10 18:33:37 -0300

    Calculadora de Reembolso
    Inicio de uma calculadora de reembolso

--- CalculadoraReembolso/CalculadoraReembolso/Form1.cs
+++ CalculadoraReembolso/CalculadoraReembolso/Form1.cs
@@ -12,9 +12,37 @@ namespace CalculadoraReembolso
 {
     public partial class Form1 : Form
     {
+        int startingMileage;
+        int endingMileage;
+
+        double milesTraveled; //Milhas viajadas - transformar em Kilometros
+        double reiburseRate = 0.39d; //Taxa de reembolso
+        double amountOwed;   //quantia devida
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            startingMileage = (int)numericUpDown1.Value;
+            endingMileage = (int)numericUpDown2.Value;
+            if (startingMileage <= endingMileage)
+            {
+                milesTraveled = endingMileage -= startingMileage;
+                amountOwed = milesTraveled *= reiburseRate;
+                label4.Text = "R$" + amountOwed;
+            }
+            else
+            {
+                MessageBox.Show("A quilometragem inicial precisa ser menor que a final", "Impossível calcular");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            milesTraveled = endingMileage -= startingMileage;
+            MessageBox.Show(milesTraveled.ToString());
         }
     }
 }
