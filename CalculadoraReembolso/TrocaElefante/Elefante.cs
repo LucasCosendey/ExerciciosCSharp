d9f62b33745a277773e72ef94b528fb7848dd71e
LucasCosendey <lcosendey@gmail.com>
2021-09-16 00:01:13 -0300

    calculadora reembolso
    e troca de corpo do elefante

---
+++ CalculadoraReembolso/TrocaElefante/Elefante.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrocaElefante
+{
+    class Elefante
+    {
+        public String name;
+        public int EarSize;
+
+        public void QuemSouEu()
+        {
+            System.Windows.Forms.MessageBox.Show("Minhas orelhas medem " + EarSize, this.name + " diz." );
+        }
+    }
+}
---
+++ CalculadoraReembolso/TrocaElefante/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrocaElefante
+{
+    public partial class Form1 : Form
+    {
+        Elefante lucinda = new Elefante() { name = "Lucinda", EarSize = 33 };
+        Elefante loyd = new Elefante() { name = "Loyd", EarSize = 40 };
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            loyd.QuemSouEu();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            lucinda.QuemSouEu();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Elefante trocado;
+            trocado = loyd;
+            loyd = lucinda;
+            lucinda = trocado;
+            MessageBox.Show("Elefantes com o corpo trocado.");
+        }
+    }
+}
