460a3b0906e3f12127eff70fefae31baa645d57c
LucasCosendey <lcosendey@gmail.com>
2021-09-21 12:31:40 -0300

    Menus
    Feito o posicionamento e animação da maioria dos menus

---
+++ CalculadoraReembolso/MenuAleatorioJoaoPodrao/MenuMaker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuAleatorioJoaoPodrao
+{
+    class MenuMaker
+    {
+        public Random Randomizer;
+        string[] carnes = {"Rosbife", "Salami", "Peru", "Presunto", "Pastrami"};
+        string[] condimentos = {"mostarda amarela", "mostarda marrom", "mostarda com mel", "maionese", "molho francês"};
+        string[] paes = {"centeio", "branco", "trigo", "pão italiano", "árabe"};
+
+        public string GetMenuItem()
+        {
+            string randomCarne = carnes[Randomizer.Next(carnes.Length)];
+            string randomCondimentos = condimentos[Randomizer.Next(condimentos.Length)];
+            string randomPaes = paes[Randomizer.Next(paes.Length)];
+            return randomCarne + " com " + randomCondimentos + " no " + randomPaes;
+
+        }
+
+    }
+}
